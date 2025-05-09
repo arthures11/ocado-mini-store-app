b1985f12d687a69e1bf96f3867eff7311f074bd1
arturito <58474213+arthures11@users.noreply.github.com>
2025-05-09 20:51:49 +0200

    displaying new notif after new product added

--- app/components/ProductCard.tsx
+++ app/components/ProductCard.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import type {Product} from '../types';
 import { useCart } from '../context/CartContext';
 import { formatPriceDisplay } from '../utils/priceUtils';
+import {useNotification} from "~/context/NotificationContext";
 
 interface ProductCardProps {
     product: Product;
@@ -9,13 +10,19 @@ interface ProductCardProps {
 
 const ProductCard: React.FC<ProductCardProps> = ({ product }) => {
     const { addToCart } = useCart();
+    const { addNotification } = useNotification();
+
+    const handleAddToCart = () => {
+        addToCart(product);
+        addNotification(`${product.name} added to cart!`, 'success');
+    };
 
     return (
         <div className="border rounded-lg p-4 shadow-lg bg-white flex flex-col">
             <h3 className="text-xl font-semibold mb-2">{product.name}</h3>
             <p className="text-gray-700 text-lg mb-4">${formatPriceDisplay(product.price)}</p>
             <button
-                onClick={() => addToCart(product)}
+                onClick={() => handleAddToCart}
                 className="mt-auto bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded transition duration-150"
             >
                 Add to Cart
